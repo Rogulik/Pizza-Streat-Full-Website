b2a9784d4f871666ff55f4d39787bdcd243d91dc
Marcin Rygielski <mrogulik@gmail.com>
2019-07-15 09:14:22 +0200

    photos

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -7,27 +7,28 @@ import '../styles/Photos.scss'
 const Photos = () => {
   
   return (
+    <div className='photos__box photo-4'>
+        <img src={chef} alt='chef' className='chef svg'/>
+      </div>
     <div className='photos__container'>
       <div className='photos__box image-1 darken-pseudo'>
         <Header title='Fresh' />
     </div>
-    <div className='photos__box photo-4'>
-        <img src={chef} alt='chef' className='chef svg'/>
+    <div className='photos__box image-5'>
+        <img src={pizza} alt='pizza' className='pizza svg'/>
       </div>
     <div className='photos__box image-2 darken-pseudo'>
       <Header title='Delicious' />
     </div>
-    <div className='photos__box image-5'>
-        <img src={pizza} alt='pizza' className='pizza svg'/>
+    <div className='photos__box image-6'>
+        <img src={smile} alt='smile' className='smile svg'/>
       </div>
     <div className='photos__box image-3 darken-pseudo'>
       <Header title='Affordable' />
     </div>
       
       
-      <div className='photos__box image-6'>
-        <img src={smile} alt='smile' className='smile svg'/>
-      </div>
+      
     </div>
   )
 }
